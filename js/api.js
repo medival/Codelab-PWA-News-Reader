bffae69044fabbb396b562ddc0f4fa5140de8ed0
Adi Purnomo <adipurnomo811@gmail.com>
2020-10-18 23:04:08 +0700

    Refactor

---
+++ js/api.js
@@ -0,0 +1,134 @@
+const baseUrl = "https://readerapi.codepolitan.com/";
+
+// Blok kode
+// function status(respnse) {
+
+// }
+
+let status = response => {
+    if (response.status !== 200) {
+        console.log(`Error : ${response.status}`);
+
+        return Promise.reject(new Error(response.statusText));
+    } else {
+        return Promise.resolve(response);
+    }
+}
+
+// Blok kode parsing json -> array
+let json = (response) => {
+    return response.json();
+}
+
+// Blok kode handle catch
+let error = (error) => {
+    // Parameter error dr Promise.reject()
+    console.log(`Error : ${error}`);
+}
+
+// Blok request data json
+function getArticles() {
+    if ("caches" in window) {
+      caches.match(baseUrl + "articles").then(function(response) {
+        if (response) {
+          response.json().then(function(data) {
+            var articlesHTML = "";
+            data.result.forEach(function(article) {
+              articlesHTML += `
+                    <div class="card">
+                      <a href="./article.html?id=${article.id}">
+                        <div class="card-image waves-effect waves-block waves-light">
+                          <img src="${article.thumbnail}" />
+                        </div>
+                      </a>
+                      <div class="card-content">
+                        <span class="card-title truncate">${article.title}</span>
+                        <p>${article.description}</p>
+                      </div>
+                    </div>
+                  `;
+            });
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("articles").innerHTML = articlesHTML;
+          });
+        }
+      });
+    }
+
+    fetch(baseUrl + "articles")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+
+      // Menyusun komponen card artikel secara dinamis
+      var articlesHTML = "";
+      data.result.forEach(function(article) {
+        articlesHTML += `
+              <div class="card">
+                <a href="./article.html?id=${article.id}">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${article.thumbnail}" />
+                  </div>
+                </a>
+                <div class="card-content">
+                  <span class="card-title truncate">${article.title}</span>
+                  <p>${article.description}</p>
+                </div>
+              </div>
+            `;
+      });
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("articles").innerHTML = articlesHTML;
+    })
+    .catch(error);
+}
+
+let getArticleById = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    let idParam = urlParams.get("id");
+
+    if ("caches" in window) {
+        caches.match(baseUrl + "article/" + idParam).then(function(response) {
+          if (response) {
+            response.json().then(function(data) {
+              var articleHTML = `
+                <div class="card">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${data.result.cover}" />
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title">${data.result.post_title}</span>
+                    ${snarkdown(data.result.post_content)}
+                  </div>
+                </div>
+              `;
+              // Sisipkan komponen card ke dalam elemen dengan id #content
+              document.getElementById("body-content").innerHTML = articleHTML;
+            });
+          }
+        });
+      }
+
+      let endPoint = `${baseUrl}article/${idParam}`;
+
+    fetch(endPoint)
+    .then(status)
+    .then(json)
+    .then((data) => {
+        console.log(data);
+        let articleHTML = `
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${data.result.cover}" />
+                </div>
+                <div class="card-content">
+                    <span class="card-title">${data.result.post_title}</span>
+                   ${snarkdown(data.result.post_content)}
+                </div>
+            </div>
+        `;
+        document.getElementById("body-content").innerHTML = articleHTML;
+    })
+}
+
---
+++ service-worker.js
@@ -0,0 +1,77 @@
+let CACHE_NAME = 'firstpwa-v4';
+var urlsToCache = [
+	'/',
+	'/article.html',
+	'/icon.png',
+	'/index.html',
+	'/manifest.json',
+	'/nav.html',
+	'/pages/home.html',
+	'/pages/about.html',
+	'/pages/contact.html',
+	'/css/materialize.css',
+	'/css/materialize.min.css',
+	'/js/api.js',
+	'/js/materialize.min.js',
+	'/js/regist.js',
+	'/js/script.js'
+];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+})
+
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches.keys()
+		.then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if(cacheName != CACHE_NAME){	
+						console.log("ServiceWorker: cache " + cacheName + " dihapus");
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+})
+
+self.addEventListener('fetch', (event) => {
+	// event.respondWith(
+	// 	caches.match(event.request, {cacheName:CACHE_NAME})
+	// 	.then((response) => {
+	// 		if(response){
+	// 			console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+	// 			return response;
+	// 		}
+			
+	// 		console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+	// 		return fetch(event.request);
+	// 	})
+	// );
+
+	const baseUrl = "https://readerapi.codepolitan.com/";
+
+	if (event.request.url.indexOf(baseUrl) > -1) {
+		event.respondWith(
+			caches.open(CACHE_NAME).then((cache) => {
+				return fetch(event.request).then((response) => {
+					cache.put(event.request.url, response.clone());
+				})
+			})
+		);
+	} else {
+		event.respondWith(
+			caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+				return response || fetch (event.request);
+			})
+		)
+	}
+});
+
